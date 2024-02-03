7dfb8fef4d11b68f467fa4397882a1c9e154ca4b
Ngwasiri-Ryan <ryanngwasiri@gmail.com>
2024-02-03 13:01:42 +0100

    updated portfolio website

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import  { useRef } from 'react';
 import './Contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
+
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ij0vsp1', 'template_ao5iith', form.current, 'ip41CGhs3MowyAGey')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
    <section id="contact">
-    contact
+    <h5>Get in Touch</h5>
+    <h2>Contact Me</h2>
+
+
+    <div className="container contact__container">
+      <div className="contact__options">
+        <article className="contact__option">
+         <MdOutlineEmail className='contact__option-icon'/>
+          <h4>Email</h4>
+          <h5>ryanngwasiri@gmail.com</h5>
+          <a href="mailto:ryanngwasiri@gmail.com">Send a message</a>
+        </article>
+
+        <article className="contact__option">
+         <RiMessengerLine className='contact__option-icon' />
+          <h4>Messenger</h4>
+          <h5>ngwasiriryantaniform</h5>
+          <a href="https://m.me/ngwasiriryantaniform">Send a message</a>
+        </article>
+
+        <article className="contact__option">
+         <BsWhatsapp className='contact__option-icon' />
+          <h4>WhatsApp</h4>
+          <h5>+237 650982351</h5>
+          <a href="https://wa.link/vxfkwx">Send a message</a>
+        </article>
+
+      </div>
+      {/***end of contact options */}
+
+      <form ref={form} onSubmit={sendEmail}>
+         <input type="text" name='name' placeholder='Your Full Name' required/>
+         <input type="email" name='email' placeholder='Your Email' required/>
+         <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+         <button type='submit' className='btn btn-primary'>Send Message</button>
+      </form>
+
+    </div>
    </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,80 @@
 import React from 'react'
 import './Testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import {  Pagination , Navigation} from 'swiper/modules';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+//import swiper slides
+
+const data=[
+  {
+    name:'Tina Snow',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quasi pariatur ad aliquam necessitatibus deserunt impedit cumque temporibus, sequi provident iusto tempora possimus dolore eligendi aspernatur eos officiis quibusdam eveniet',
+    image:AVTR1,
+  },
+  {
+    name:'Tina Snow',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quasi pariatur ad aliquam necessitatibus deserunt impedit cumque temporibus, sequi provident iusto tempora possimus dolore eligendi aspernatur eos officiis quibusdam eveniet',
+    image:AVTR2,
+  },
+  {
+    name:'Tina Snow',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quasi pariatur ad aliquam necessitatibus deserunt impedit cumque temporibus, sequi provident iusto tempora possimus dolore eligendi aspernatur eos officiis quibusdam eveniet',
+    image:AVTR3,
+  },
+  {
+    name:'Tina Snow',
+    review:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, quasi pariatur ad aliquam necessitatibus deserunt impedit cumque temporibus, sequi provident iusto tempora possimus dolore eligendi aspernatur eos officiis quibusdam eveniet',
+    image:AVTR4,
+  },
+]
 const Testimonials = () => {
   return (
-   <section id='#testimonials'>testimonials</section>
+   <section id='#testimonials'>
+    <h5>Review from clients</h5>
+    <h2>Testimonials</h2>
+    <Swiper className=" container testimonials__container"
+
+     // install Swiper modules
+     modules={[ Pagination, Navigation ]}
+     spaceBetween={40}
+     slidesPerView={1}
+     pagination={{ clickable: true }}
+
+    >
+       {
+          data.map(({ name,review,image },index) => {
+            return(
+              <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={image} alt="avatar" />
+              </div>
+              <h5>{name}</h5>
+                <small className="client__review">
+                 {review}
+              </small>
+              </SwiperSlide>
+            )
+          })
+        }
+    </Swiper>
+
+   </section>
   )
 }
 
 export default Testimonials
+
+
